5dd7ce92c94768836615bd0a46ab4c27552ab067
Kieran Finn <kieran.finn@hotmail.com>
2022-07-17 16:35:18 +0100

    Add project files.

---
+++ PhotoBackend/MediaFile.cs
@@ -0,0 +1,13 @@
+namespace PhotoBackend
+{
+    public class MediaFile
+    {
+        public string URL { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
