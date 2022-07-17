f3d7053895972e500fb043e7660acd09a8537cd2
Kieran Finn <kieran.finn@hotmail.com>
2022-07-17 18:10:51 +0100

    added more BQ tests

--- PhotoBackend/Controllers/TestBQController.cs
+++ PhotoBackend/Controllers/TestBQController.cs
@@ -40,5 +40,50 @@ namespace PhotoBackend.Controllers
             }
             return output;
         }
+
+        [HttpGet("simple", Name = "GetSimpleTestData")]
+        public List<string> TestSimpleQuery(
+        string projectId = "photosharesite"
+    )
+        {
+            BigQueryClient client = BigQueryClient.Create(projectId);
+            BigQueryTable table = client.GetTable("bigquery-public-data", "samples", "shakespeare");
+
+            string sql = $"SELECT corpus AS title, COUNT(word) AS unique_words FROM {table} GROUP BY title ORDER BY unique_words DESC LIMIT 10";
+            BigQueryParameter[] parameters = null;
+            BigQueryResults results = client.ExecuteQuery(sql, parameters);
+            List<string> output = new List<string>();
+
+            foreach (BigQueryRow row in results)
+            {
+                output.Add($"{row["title"]}: {row["unique_words"]}");
+            }
+            return output;
+        }
+
+        [HttpGet("parametrize", Name = "GetParametrizedTestData")]
+        public List<string> TestParametrizedQuery(
+        string projectId = "photosharesite"
+    )
+        {
+            BigQueryClient client = BigQueryClient.Create(projectId);
+            BigQueryTable table = client.GetTable("bigquery-public-data", "usa_names", "usa_1910_2013");
+
+            string sql = $"SELECT name FROM {table} WHERE state = @state LIMIT 100";
+
+            List<string> output = new List<string>();
+            BigQueryParameter[] parameters = new[]
+            {
+                new BigQueryParameter("state", BigQueryDbType.String, "NY"),
+            };
+            BigQueryResults results = client.ExecuteQuery(sql, parameters);
+            
+
+            foreach (BigQueryRow row in results)
+            {
+                output.Add(Convert.ToString(row["name"]));
+            }
+            return output;
+        }
     }
 }
